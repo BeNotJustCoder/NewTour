7fbcad2ba9a7a6fe690cbbfff51827bebf15881a
BeNotJustCoder <benotjustcoder@163.com>
2015-05-15 22:45:27 +0800

    Initial Commit

---
+++ NewTour/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTour
+//
+//  Created by lincy on 15/5/15.
+//  Copyright (c) 2015年 lincy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewTour/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTour
+//
+//  Created by lincy on 15/5/15.
+//  Copyright (c) 2015年 lincy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
